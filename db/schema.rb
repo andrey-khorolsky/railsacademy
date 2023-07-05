4dcbee9fbcb46969f685e7175aaab3bb38f38532
andrew <horolskyandrey@gmail.com>
2023-07-05 23:21:47 +0300

    db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_05_141651) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_05_192449) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,11 +39,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_141651) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "followers", force: :cascade do |t|
+    t.integer "author_id"
+    t.integer "follower_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "posts", force: :cascade do |t|
     t.integer "user_id"
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "img"
   end
 
   create_table "users", force: :cascade do |t|
@@ -61,5 +69,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_05_141651) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "followers", "users", column: "author_id"
+  add_foreign_key "followers", "users", column: "follower_id"
   add_foreign_key "posts", "users"
 end

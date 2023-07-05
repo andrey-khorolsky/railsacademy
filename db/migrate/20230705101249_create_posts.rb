f7bfb052b9fd637b6fb9af0731a576b9bb920f42
andrew <horolskyandrey@gmail.com>
2023-07-05 17:09:14 +0300

    db for post

--- db/migrate/20230705101249_create_posts.rb
+++ db/migrate/20230705101249_create_posts.rb
@@ -2,10 +2,13 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
 
-      t.int :author_id,
-      t.text :text,
-      
+      t.integer :user_id
+      t.text :text
+      # t.attachment :img, default: nil
+
       t.timestamps
     end
+
+    add_foreign_key :posts, :users
   end
 end

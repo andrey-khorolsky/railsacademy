161f7fc33a28c1be8747ffc1be1a54a7c203a3c0
andrew <horolskyandrey@gmail.com>
2023-07-05 21:41:45 +0300

    unfollow to users

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
 class UserController < ApplicationController
-    before_action :authenticate_user!
-    
+  before_action :authenticate_user!
+
   def index
     @user = User.all
   end
@@ -14,22 +14,17 @@ class UserController < ApplicationController
 
   def show
     @user = User.find(params['id'])
-    @follower = Follower.where(['author_id = ? and follower_id = ?', @user.id, current_user.id]).count
-    # binding.pry
+    @follower = Follower.where(['author_id = ? and follower_id = ?', @user.id, current_user.id]).count == 1
   end
 
   def follow
     Follower.new(author_id: params['id'], follower_id: current_user.id).save
-    # binding.pry
 
     redirect_to '/user/' + params['id']
   end
 
   def unfollow
-    @f = Follower.where(['author_id = ? and follower_id = ?', params['id'], current_user.id])['id']
-    # @f.destroy
-    binding.pry
+    Follower.delete_by(author_id: params['id'], follower_id: current_user.id)
     redirect_to '/user/' + params['id']
   end
-
 end

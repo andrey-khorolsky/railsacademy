f47011af9be15b0f70b9f3d8b1dbcd8bdd2d98aa
andrew <horolskyandrey@gmail.com>
2023-07-05 17:43:08 +0300

    post (contr, ind, new)

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,2 +1,16 @@
 class PostController < ApplicationController
+
+    def index
+        @post = Post.all
+    end
+
+    def create
+        @post = Post.new(params.require(:post).permit(:text, :img))
+        @post.author_id = current_user.id
+        @post.save
+    end
+
+    def show
+        @post = Post.find(params['id'])
+    end
 end

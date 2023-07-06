e29530d01e2f189caf5bfab5f4c6e12ca24ccdd5
andrew <horolskyandrey@gmail.com>
2023-07-06 13:24:58 +0300

    view self likes and upd news (and img)

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,6 +1,10 @@
 class NewsController < ApplicationController
   def index
-    @authors = Follower.select(:author_id).where('follower_id = ?', current_user.id)
-    @posts = Post.where('user_id IN (?)', @authors).order(created_at: :desc)
+    if user_signed_in?
+        @authors = Follower.select(:author_id).where('follower_id = ?', current_user.id)
+        @posts = Post.where('user_id IN (?)', @authors).order(created_at: :desc)
+    else
+        @posts = Post.all
+    end
   end
 end

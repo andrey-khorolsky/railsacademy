616c7d7f81d3e39531086a46dc173230ad05aadc
andrew <horolskyandrey@gmail.com>
2023-07-10 18:58:50 +0300

    like tests

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,156 +1,119 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
-
-  describe "#findLikes" do
-
+  describe '#findLikes' do
     subject { Like.findLikes post_id }
 
-
-    context "post has some likes" do
-
-      let (:post_id) { 4 }
-
-      it "find post's likes" do
-        expect(subject).to eq(2)
-      end
+    context 'post has some likes' do
+      let(:post_id) { 4 }
+      it { is_expected.to eq 2 }
     end
-
 
     context "post hasn't like" do
-
-      let (:post_id) { 1 }
-
-      it "find post's likes" do
-        expect(subject).to eq(0)
-      end
+      let(:post_id) { 1 }
+      it { is_expected.to eq 0 }
     end
 
-
-    context "post not exist" do
-
-      let (:post_id) { 0 }
-
-      it "find post's likes" do
-        expect(subject).to eq(0)
-      end
+    context 'post not exist' do
+      let(:post_id) { 0 }
+      it { is_expected.to eq 0 }
     end
-
-    
   end
 
-  describe "#arePostLikedBy" do
+  describe '#arePostLikedBy' do
+    subject { Like.arePostLikedBy post_id, user_id }
+    let(:post_id) { 3 }
 
-    subject {Like.arePostLikedBy post_id, user_id}
-    let (:post_id) { 3 }
+    context 'user are liked post' do
+      let(:user_id) { 1 }
 
-    context "user are liked post" do
-
-      let (:user_id) { 1 }
-      let (:res) { true }
-
-      it {is_expected.to eq res}
+      it { is_expected.to eq true }
     end
 
     context "user don't liked post" do
+      let(:user_id) { 3 }
 
-      let (:user_id) { 3 }
-      let (:res) { false }
-
-      it {is_expected.to eq res}
+      it { is_expected.to eq false }
     end
 
     context "user or post don't exist" do
+      it { expect(Like.arePostLikedBy(1, 200)).to eq false }
 
-      let (:res) { false }
-      
-      let (:user_id) { 200 }
-
-      it  {is_expected.to eq res}
-
-      let (:user_id) { 1 }
-      let (:post_id) { 200 }
-
-      it {is_expected.to eq res}
+      it { expect(Like.arePostLikedBy(200, 1)).to eq false }
     end
   end
 
-  describe "#like" do
+  describe '#like' do
+    subject { Like.like post_id, user_id }
+    let(:post_id) { 4 }
 
-    subject{ Like.like post_id, user_id}
+    context 'user like post' do
+      let(:user_id) { 1 }
 
-    context "user like post" do
-      let (:user_id) { 1 }
-      let (:post_id) { 4 }
-      let (:res) { true }
+      it { is_expected.to eq true }
 
-      it "user first time like post" do
-        expect(subject).to eq res
-      end
-
-      it "user second time like post" do
+      it 'user second time like post' do
         Like.like post_id, user_id
-        expect(subject).not_to eq res
+        expect(subject).to eq false
       end
+
+      # it { should validate_uniqueness_of(:user_id).scoped_to(:post_id)}
     end
 
-    context "user or post don't exist" do
-
-      let (:user_id) { 200 }
+    context "user don't exist" do
+      let(:user_id) { 200 }
       it "user don't exist" do
-        expect{subject}.to raise_error
+        expect { subject }.to raise_error
       end
 
-      let (:post_id) { 200 }
-      let (:user_id) { 1 }
+      # it {is_expected.to raise_error}
+    end
+
+    context "post don't exist" do
+      let(:post_id) { 200 }
+      let(:user_id) { 1 }
       it "post don't exist" do
-        expect{subject}.to raise_error
+        expect { subject }.to raise_error
       end
+      # it { is_expected.to raise_error}
     end
   end
 
-  describe "#dislike" do
+  describe '#dislike' do
+    subject { Like.dislike post_id, user_id }
 
-    subject{ Like.dislike post_id, user_id}
+    let(:post_id) { 3 }
+    let(:user_id) { 1 }
+    let(:res) { 1 }
 
-    let (:post_id) { 3 }
-    let (:user_id) { 1 }
-    let (:res) { 1 }
-
-    context "user dislike post" do
-
-      it "user first time dislike post" do
+    context 'user dislike post' do
+      it 'user first time dislike post' do
         expect(subject).to eq res
       end
 
-      it "user second time dislike post" do
+      it 'user second time dislike post' do
         Like.dislike post_id, user_id
         expect(subject).to eq 0
       end
 
-      it "do some likes and one dislike" do
+      it 'do some likes and one dislike' do
         Like.like post_id, user_id
         Like.like post_id, user_id
         expect(subject).to eq res
       end
     end
 
-    context "user or post don't exist" do
+    context "user don't exist" do
+      let(:user_id) { 200 }
 
-      let (:user_id) { 200 }
-      
-      it "user don't exist" do
-        expect(subject).to eq 0
-      end
+      it { is_expected.to eq 0 }
+    end
 
-      let (:post_id) { 200 }
-      let (:user_id) { 1 }
+    context "post don't exist" do
+      let(:post_id) { 200 }
+      let(:user_id) { 1 }
 
-      it "post don't exist" do
-        expect(subject).to eq 0
-      end
+      it { is_expected.to eq 0 }
     end
   end
-
 end

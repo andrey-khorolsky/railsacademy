99412c3161194a75678d57efff582bd6d36b852e
andrew <horolskyandrey@gmail.com>
2023-07-12 12:31:00 +0300

    upd account and show post

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -2,35 +2,38 @@ class PostController < ApplicationController
   before_action :authenticate_user!
   rescue_from ActiveRecord::InvalidForeignKey, with: :redirect_error
 
-  def index
-    @post = Post.findPostsBy current_user.id
-  end
-
+  # Create new post
   def create
     Post.new(text: params[:post][:text], img: params[:post][:img], user_id: current_user.id).save
     redirect_to :post_index
   end
 
+  # Show selected post
   def show
     @post = Post.find(params['id'])
   end
 
+  # Like post by current user
   def like
     Like.like params['id'], current_user.id
     redirect_back fallback_location: root_path
   end
 
+  # Dislike post by current user
   def dislike
     Like.dislike params['id'], current_user.id
     redirect_back fallback_location: root_path
   end
 
+  # Create comment to post by current user
   def addComment
     Comment.new(text: params[:comment][:text], post_id: params[:id], user_id: current_user.id).save
     redirect_back fallback_location: root_path
   end
 
   private
+
+  # Redirect to /post if error
   def redirect_error
     redirect_back fallback_location: root_path
   end

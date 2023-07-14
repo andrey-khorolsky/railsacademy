7d6ca73c5d014f467432023bc46abe4d6985fd0f
andrew <horolskyandrey@gmail.com>
2023-07-14 11:11:58 +0300

    db and all users view

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# 10.times do
+#     user = FactoryBot.build(:user)
+#     rand(1..5).times do
+#         Like.like rand(1..10), user.id
+#     end
+#     rand(1..5).times do
+#         Comment.new(post_id: rand(1..10), user_id: user.id, text: Faker::Lorem.sentence)
+#     end
+#     user.save
+# end
+
+# 16.times do |id|
+#     Follower.follow rand(1..16),rand(1..16)
+# end
+
+# 16.times do
+    # Like.like rand(1..26), rand(5..16)
+    # Comment.new(post_id: rand(1..26), user_id: rand(5..16), text: Faker::Lorem.sentence).save
+# end
